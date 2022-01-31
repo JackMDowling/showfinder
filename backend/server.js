18ed1d993896dbdbf2b05731e2650f9c879ba705
Jack <jack.m.dowling26@gmail.com>
2022-01-31 16:08:44 -0500

    trimming

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,5 @@ const app = express();
 // instantiate app
 app.use(express.json());
 app.use(express.static(path.join("../../client/dist")));
-const server = require("http").createServer(app);
 
 app.listen(3113, console.log("server connected"));
