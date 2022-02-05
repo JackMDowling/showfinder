1d13f1df28b51a1634d77f8ddb30dbc7ea99238c
Jack <jack.m.dowling26@gmail.com>
2022-02-05 16:32:13 -0500

    Now gets your followed artists and stores them in state

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,6 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import regeneratorRuntime from "regenerator-runtime";
 
 
 const getParamsFromSpotify = (hash) => {
@@ -13,25 +15,49 @@ const getParamsFromSpotify = (hash) => {
 }
 
 export const App = function () {
+    const [token, setToken] = useState('')
+    const [artists, setArtists] = useState([])
 
-    React.useEffect(() => {
+   useEffect( () => {
         if(window.location.hash) {
         const { access_token, 
         expires_in,
         token_type
         } = getParamsFromSpotify(window.location.hash)
-            console.log(access_token)
-            localStorage.clear();
-            localStorage.setItem("accessToken", access_token);
-            localStorage.setItem("tokenType", token_type);
-            localStorage.setItem('expiresIn', expires_in)
+        setToken(access_token)
+           
         }
-    })
+    }, [])
 
-    return (
-        <div className="app">
-              <a href="/login" className="login-btn">Log in with Spotify</a>
-        </div>
-    )
+    const findArtists = async () => {
+        const authString = 'Bearer ' + token;
+         const { data } = await axios.get('https://api.spotify.com/v1/me/following?type=artist&limit=50', {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': authString
+                }
+            });
+            
+            const { artists } = data
+            const { items } = artists
+            if(items) {
+                const store = [];
+                for (let i = 0; i < items.length; i ++) {
+                    store.push(items[i].name)
+                }
+                setArtists(store)
+                console.log(store)
+                
+            }
+    }
+        
+        
+            return (
+                <div className="app">
+                      <a href="/login" className="login-btn">Log in with Spotify</a><br></br>
+                      <button onClick={findArtists}>Find Artists</button>
+                </div>
+            )
+    }
 
-}
